c1df9dc7364c18a28d8a0707b28d2627d94f2ae6
Stasys Meclazcke <aeip@live.com>
2022-01-15 16:04:47 -0600

    styling for swift code centered and support for more

--- src/Components/SwiftCode.js
+++ src/Components/SwiftCode.js
@@ -7,8 +7,10 @@ const SwiftCode = () => {
 		<div id="swiftcode">
 			<h2>Swift Code</h2>
             <div className="swift-code">
-                <Gist id="47b7416d0d2cb7cb37b549e45d04d09f" />
-                <img height="350px" src={LoginRegister} alt='Login/Register' />
+                <div className="swift-code-container">
+                    <img height="350px" src={LoginRegister} alt='Login/Register' />
+                    <Gist id="47b7416d0d2cb7cb37b549e45d04d09f" />
+                </div>
             </div>
 		</div>
 	);
