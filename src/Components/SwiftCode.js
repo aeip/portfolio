6bf2484f85a3ac4dbd15cc2f96a60bcef164fb3e
Stasys Meclazcke <aeip@live.com>
2022-01-14 23:51:21 -0600

    Uploaded 1 example of swift code

--- src/Components/SwiftCode.js
+++ src/Components/SwiftCode.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Gist from "react-gist";
+import LoginRegister from '../img/LoginRegister.png'
 
 const SwiftCode = () => {
 	return (
 		<div id="swiftcode">
 			<h2>Swift Code</h2>
-			
+            <div className="swift-code">
+                <Gist id="47b7416d0d2cb7cb37b549e45d04d09f" />
+                <img height="350px" src={LoginRegister} alt='Login/Register' />
+            </div>
 		</div>
 	);
 };
